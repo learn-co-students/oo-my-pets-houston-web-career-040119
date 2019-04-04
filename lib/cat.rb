a64694d507ec4adbb151ab5bfa9f399069fcd8e8
smrbts <samueljustinroberts@gmail.com>
2019-04-04 15:14:42 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
+
+require_relative("owner.rb")
+
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+
+  end
+
+  def name
+    @name
+  end
+  
 end
