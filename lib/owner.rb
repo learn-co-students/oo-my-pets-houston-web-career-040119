ea533df1bdcfb8777f08932f32062fd6b82c62c1
github username <github email address>
2019-04-04 17:18:44 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,93 @@
+
+require 'pry'
 class Owner
-  # code goes here
+
+attr_accessor :name, :pets
+attr_reader :species
+OWNERS = []
+
+def initialize(name)
+    @name = name
+    @species = "human"
+    OWNERS << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
 end
+
+def self.all
+  OWNERS
+end
+
+def self.count
+  OWNERS.length
+end
+
+def self.reset_all
+  OWNERS.clear
+end
+
+
+    def buy_cat(name)
+      @pets[:cats] << Cat.new(name)
+    end
+       
+    def buy_dog(name)
+      @pets[:dogs] << Dog.new(name)
+    end
+            
+    def buy_fish(name)
+      @pets[:fishes] << Fish.new(name)
+    
+    end
+
+    def walk_dogs
+
+      @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+      end
+
+    end
+
+    def play_with_cats
+      @pets[:cats].each do |cat|
+        cat.mood = "happy"
+      end
+    end
+
+    def feed_fish
+      @pets[:fishes].each do |fish|
+        fish.mood = "happy"
+    end
+  end
+
+     def say_species
+      "I am a human."
+     end
+    
+  def sell_pets
+    # @pets.mood = "nervous"
+
+    @pets.each do |type, animals | 
+ 
+      animals.each do |animal|
+        animal.mood = "nervous"
+      end
+    end
+
+    @pets.clear
+  
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+  end
+    
+    
+    
+    end
+   
+   
+
+    
+
+
+
